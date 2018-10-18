b64c9952fd40e90734d73a5bcf0a1789853a54a9
theresamorelli <theresa.l.morelli@gmail.com>
2018-10-18 14:21:52 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0; i < rankedLists.length; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0; j < children.length; j++) {
+      let child = children[j];
+
+      child.innerHTML = parseInt(child.innerHTML) + n;
+    }
+  }
+}
